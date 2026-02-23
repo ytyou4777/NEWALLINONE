c06fb07f5616a66033165b939e395ff80f843d2f
ytyou4777 <ytyou4777@gmail.com>
2026-02-23 23:36:01 +0530

    Update update-playlist.js

--- update-playlist.js
+++ update-playlist.js
@@ -11,7 +11,7 @@ const SOURCES = {
   SONYLIV_M3U: "https://raw.githubusercontent.com/ytyou4777/sony-playlist/refs/heads/main/SONY.m3u", // updated
   FANCODE_JSON: "https://raw.githubusercontent.com/drmlive/fancode-live-events/main/fancode.json",
   ICC_TV_JSON: "https://icc.vodep39240327.workers.dev/icctv.jso",
-  SPORTS_JSON: "https://raw.githubusercontent.com/ytyou4777/SPORTS/refs/heads/main/index.m3u",
+  SPORTS_JSON: "",
 };
 
 // ================= PLAYLIST HEADER =================
